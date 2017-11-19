419d3fd7dd92d420260fe11aebe503933c2f4b99
TWJolly <tobiasjolly@gmail.com>
2017-11-19 17:11:05 +0000

    create raw_data csv with file name column

--- create_csv_from_html_emails.R
+++ create_csv_from_html_emails.R
@@ -3,22 +3,25 @@ library(purrr)
 library(dplyr)
 library(tidyr)
 
+emails_folder <- 'emails'
 jobs_xpath <- '//p | //h3 | //h2'
 column_names = c("job_title", "department", "location", "salary", "grade", "approach", "role_type","closing_date")
+raw_data_csv_name <- 'raw_data.csv'
 
-emails_folder <- 'emails'
-data <- emails_folder %>%
-  dir(pattern = "*.html") %>%
-  map(~read_html(paste(emails_folder,.,sep ='\\'))) %>%
-  map(~html_nodes(., xpath = jobs_xpath)) %>%
-  map(html_text) %>%
-  map(~tail(., -2)) %>%
-  map(~head(., -1)) %>%
-  map(~data.frame('all_data' = ., 
-                  'id' = rep(1:(length(.)%/%8), each = 8),
-                  stringsAsFactors = F)) %>%
-  map(~group_by(., id)) %>%
-  map(~summarise(., 'all_data' = paste(all_data, collapse ="!!!"))) %>%
-  map(~separate(., 'all_data', into = column_names, sep = "!!!")) %>%
-  reduce(bind_rows)
-
+data_frame(filename = dir(emails_folder, pattern = "*.html")) %>%
+  mutate(date_downloaded = gsub(".html", "", filename),
+         file_contents = map(filename, ~ read_html(paste(emails_folder,.,sep ='\\'))) %>% 
+           map(~html_nodes(., xpath = jobs_xpath)) %>%
+           map(html_text) %>%
+           map(~tail(., -2)) %>%
+           map(~head(., -1)) %>%
+           map(~data.frame('all_data' = ., 
+                           'id' = rep(1:(length(.)%/% length(column_names)), each = length(column_names)),
+                           stringsAsFactors = F))) %>%
+  unnest %>%
+  group_by(id, date_downloaded) %>%
+  summarise('all_data' = paste(all_data, collapse ="!!!")) %>%
+  separate('all_data', into = column_names, sep = "!!!") %>% 
+  ungroup() %>%
+  select(-id) %>%
+  write.csv(raw_data_csv_name, row.names = F)
