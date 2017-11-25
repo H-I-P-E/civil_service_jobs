e6eb7153d401cc2b1e591d5bdd107b56a0623b29
TWJolly <tobiasjolly@gmail.com>
2017-11-25 15:31:08 +0000

    Updated script to extract some many-to-one tables (salary, grade and role types).
    Plust some more basic data cleaning

--- clean_data.R
+++ clean_data.R
@@ -3,16 +3,43 @@ library(lubridate)
 library(stringr)
 
 raw_data_csv_name <- 'data\\raw_data.csv'
-cleaned_data_csv <- 'data\\cleaned_data.csv'
+cleaned_data_csv <- 'data\\cleaned_advert_data.csv'
+role_data_csv <- 'data\\role_data.csv'
+grade_data_csv <- 'data\\grade_data.csv'
+salary_data_csv <- 'data\\salary_data.csv'
 
 if(!file.exists(raw_data_csv_name)){
   source('create_csv_from_html_emails.R')
 }
 
-read_csv(raw_data_csv_name) %>%
-  mutate(closing_date = gsub("Closing Date: ", "", closing_date)) %>%
-  mutate(closing_date = dmy(closing_date)) %>%
-  mutate(date_downloaded = ymd(date_downloaded)) %>%
-  mutate(grade = gsub("Grade: ", "", grade)) %>%
-  mutate(approach = gsub("Approach: ", "", approach)) %>%
-  write.csv(cleaned_data_csv, row.names = F)
+all_advert_data <- read_csv(raw_data_csv_name) %>%
+  mutate(closing_date = dmy(gsub("Closing Date: ", "", closing_date)),
+         date_downloaded = ymd(date_downloaded),
+         grade = gsub("Grade: ", "", grade),
+         approach = gsub("Approach: ", "", approach),
+         location = gsub("Location: ", "", location),
+         salary = gsub("Salary: ", "", salary),
+         role_type = sub("Role Type: ", "", role_type),
+         job_id = str_extract(link, '[[:digit:]]{7,}')) %>%
+         write_csv(cleaned_data_csv)
+
+role_types <- all_advert_data %>%
+  subset(select = c(job_id, role_type)) %>%
+  mutate(role_type = strsplit(role_type, ",")) %>% 
+  unnest(role_type) %>%
+  write_csv(role_data_csv)
+
+grades <-  all_advert_data %>%
+  subset(select = c(job_id, grade)) %>%
+  mutate(grade = strsplit(grade, ",")) %>% 
+  unnest(grade) %>%
+  mutate(grade = trimws(grade)) %>%
+  write_csv(grade_data_csv)
+
+salaries <-  all_advert_data %>%
+  subset(select = c(job_id, salary)) %>%
+  mutate(salary = str_extract_all(salary, "[:digit:]{2,3}.?,?[:digit:]{3}")) %>% 
+  unnest(salary) %>%
+  mutate(salary = as.numeric(gsub('\\.|,', '', salary))) %>%
+  write_csv(salary_data_csv)
+
