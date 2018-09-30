2a2444329027dcbbed5e55f8d851689e4a99a18a
TWJolly <tobiasjolly@gmail.com>
2018-09-30 23:21:16 +0100

    updated key word search to only search for a key word in a job the first time the script is run

--- R_analysis/find_adverts_with_keys_words.R
+++ R_analysis/find_adverts_with_keys_words.R
@@ -7,10 +7,18 @@ key_words <- key_words_csv %>%
   read_csv %>%
   mutate(dummy = TRUE)
 
+
+if(file.exists(key_words_results_file)){
+  previous_key_words <- read_csv(key_words_results_file) %>%
+    mutate_all(as.character) %>%
+    mutate(ref_word = paste(job_ref, `Search term`))
+} else{previous_key_words <-NULL}
+
 key_word_counts <- all_advert_data %>%
   left_join(key_words) %>%
+  mutate(ref_word = paste(as.character(job_ref), `Search term`)) %>%
+  filter(!(ref_word %in% previous_key_words$ref_word)) %>%
   mutate(match = str_count(tolower(`Job description`),`Search term`)) %>%
-  filter(match > 0) %>%
   mutate(match_rating = match*`Strength of association 1-9 (currently subjective)`) %>%
   group_by(`Cause area`, job_ref) %>%
   mutate(cause_area_sum = sum(match_rating)) %>%
@@ -37,6 +45,8 @@ key_word_counts <- all_advert_data %>%
 #   do(create_cause_area_folder(.))
 
 key_word_counts %>%
+  mutate_all(as.character) 
+  bind_rows(previous_key_words) %>%
   write_csv(key_words_results_file)
 
 ####mining prep####
