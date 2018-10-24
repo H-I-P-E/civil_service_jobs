40f98b7a5be9b943678c80e35a74ff3a89d7b80d
TWJolly <tobiasjolly@gmail.com>
2018-10-25 00:30:13 +0100

    Added some more postcodes
    Made keywords function more efficient (removed pointless inner join and gather)

--- R_analysis/find_adverts_with_keys_words.R
+++ R_analysis/find_adverts_with_keys_words.R
@@ -7,15 +7,15 @@ key_words <- key_words_csv %>%
   read_csv
 
 count_key_word <- function(key_word, job_descriptions){
-  print(key_word)
-  job_descriptions[key_word] <- str_count(job_descriptions$`Job description`, key_word) 
+  print(key_word) #use_mutate
+  job_descriptions["search_term"] <- key_word
+  job_descriptions["count"] <- str_count(job_descriptions$`Job description`, key_word)
   return(job_descriptions  %>%
+           filter(count > 0) %>%
            select(-`Job description`))}
   
 key_word_results <- map(key_words$`Search term`, count_key_word, all_advert_data) %>%
-  reduce(inner_join, by = c("job_ref" = "job_ref")) %>%
-  gather(key = "search_term", value = "count", -job_ref) %>%
-  filter(count > 0) %>%
+  reduce(bind_rows) %>%
   left_join(key_words, by = c("search_term" = "Search term")) %>%
   mutate(match_rating = count*`Strength of association 1-9 (currently subjective)`) %>%
   group_by(`Cause area`, job_ref) %>%
