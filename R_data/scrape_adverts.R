9e2d7b1cb334ef621d9967889fa9c36785335f6d
TWJolly <tobiasjolly@gmail.com>
2019-03-14 22:34:12 +0000

    remove sleep
    corrected issues with duplciate html files

--- R_data/scrape_adverts.R
+++ R_data/scrape_adverts.R
@@ -2,12 +2,15 @@ source('user_name_and_password.R') #This file will need to contain your csj user
 
 my_session <- log_in_session(username, password)
 
-existing_files <- list.files(adverts_folder, pattern = "*.html", recursive = TRUE)
+existing_files <- list.files(adverts_folder, pattern = "*.html", recursive = TRUE, include.dirs = FALSE) %>%
+  map(~str_extract(.,"[:digit:]{7,}.html"))
+
+existing_ids <- existing_files %>% map(~str_extract(.,"[:digit:]{7,}"))
 
 download_page_logged_in <- Vectorize(function(job_ref,date_downloaded){
   folder <- file.path(adverts_folder, 
                       year(date_downloaded), 
-                      month(date_downloaded, label = TRUE))  
+                      month(date_downloaded, label = TRUE)) 
   file_name <- paste(job_ref, 'html', sep = '.')
   if(!(file_name %in% existing_files)){
     print(paste("Downloading job:",job_ref))
@@ -17,11 +20,12 @@ download_page_logged_in <- Vectorize(function(job_ref,date_downloaded){
     job_url_session <- jump_to(my_session, job_url)
     job_html <- read_html(job_url_session)
     write_xml(job_html, file_path)
-    Sys.sleep(1)}
+    }
   return(file_name)
 })
 
 curlSetOpt(timeout = 20000)
 all_adverts <- read_csv(cleaned_data_csv) %>%
   select(job_id, date_downloaded) %>%
+  filter(!(job_id %in% existing_ids)) %>%
   mutate(full_advert_file = download_page_logged_in(job_id, date_downloaded))
